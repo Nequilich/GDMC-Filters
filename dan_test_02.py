4297221a1a3cca052a114e761de0fca2f619dc77
nequilich <nequilich@gmail.com>
2019-02-07 20:45:29 +0100

    Updated dan_test_02
    It now makes pyramids

--- dan_test_02.py
+++ dan_test_02.py
@@ -34,36 +34,32 @@ def getSections(box, maxHeight):
 	currentmaxz = box.maxz
 	currentminy = box.miny + 1
 	currentmaxy = box.miny + 4
-	while(currentmaxy <= maxHeight):
+	while(currentmaxy <= maxHeight and currentmaxx - currentminx >= 3 and currentmaxz - currentminz >= 3):
 		sections.append(Section(currentminx, currentmaxx, currentminz, currentmaxz, currentminy, currentmaxy))
+		currentminx += 1
+		currentmaxx -= 1
+		currentminz += 1
+		currentmaxz -= 1
 		currentminy += 3
 		currentmaxy += 3
 	return sections
 
 def generateFloor(level, box):
-	fillArea(level, 4, box.minx, box.maxx, box.minz, box.maxz, box.miny, box.miny + 1)
+	fillArea(level, (4, 0), box.minx, box.maxx, box.minz, box.maxz, box.miny, box.miny + 1)
 
 def generateSections(level, sections):
-	num = 0
-	bt = 41
 	for section in sections:
 		clearSection(level, section)
-		fillArea(level, bt, section.minx, section.maxx, section.minz, section.maxz, section.miny, section.maxy)
-		# temporary code below
-		num += 1
-		if(num % 2 == 0):
-			bt = 41
-		else:
-			bt = 42
+		fillArea(level, (43, 5), section.minx, section.maxx, section.minz, section.maxz, section.miny, section.maxy)
 
 def clearSection(level, section):
 	clearArea(level, section.minx, section.maxx, section.minz, section.maxz, section.miny, section.maxy)
 
 def clearArea(level, minx, maxx, minz, maxz, miny, maxy):
-	fillArea(level, 0, minx, maxx, minz, maxz, miny, maxy)
+	fillArea(level, (0, 0), minx, maxx, minz, maxz, miny, maxy)
 
 def fillArea(level, blockType, minx, maxx, minz, maxz, miny, maxy):
 	for x in range(minx, maxx):
 		for z in range(minz, maxz):
 			for y in range(miny, maxy):
-					utilityFunctions.setBlock(level, (blockType, 0), x, y, z)
+					utilityFunctions.setBlock(level, blockType, x, y, z)
