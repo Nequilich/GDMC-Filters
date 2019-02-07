de561ff5e9801ab24e24cd2d90fc14f71fc4da98
Nekaerth <lasse.j.lundin@gmail.com>
2019-02-07 17:01:54 +0100

    Created my first test file
    Sets glass above the surface

---
+++ las_test_01.py
@@ -0,0 +1,14 @@
+#level.blockAt(x, y, z)
+#level.setBlockAt(x, y, z, id)
+
+def perform(level, box, options):
+	for x in range(box.minx, box.maxx):
+		for z in range(box.minz, box.maxz):
+			surfaceIsFound = False
+			y = box.maxy - 1
+			while (not surfaceIsFound):
+				if (level.blockAt(x, y, z) != 0):
+					surfaceIsFound = True
+				else:
+					y -= 1
+			level.setBlockAt(x, y + 1, z, 20)
