8dc4cd6bf2b994b10c2fc4cd4e02bc866c6cd5e0
nequilich <nequilich@gmail.com>
2019-02-11 04:38:15 +0100

    Update test_pyramid_01.py

--- test_pyramid_01.py
+++ test_pyramid_01.py
@@ -25,33 +25,47 @@ class Section:
 		self.maxz = maxz
 		self.miny = miny
 		self.maxy = maxy
+	
+	def setSection(self, section):
+		self.minx = section.minx
+		self.maxx = section.maxx
+		self.minz = section.minz
+		self.maxz = section.maxz
+		self.miny = section.miny
+		self.maxy = section.maxy
+
 
 def getSections(box, maxHeight):
 	sections = []
-	currentminx = box.minx
-	currentmaxx = box.maxx if (box.maxx - box.minx) % 2 == 1 else box.maxx - 1
-	currentminz = box.minz
-	currentmaxz = box.maxz if (box.maxz - box.minz) % 2 == 1 else box.maxz - 1
-	currentminy = box.miny + 1
-	currentmaxy = box.miny + 4
-	while(currentmaxy <= maxHeight and currentmaxx - currentminx >= 3 and currentmaxz - currentminz >= 3):
-		sections.append(Section(currentminx, currentmaxx, currentminz, currentmaxz, currentminy, currentmaxy))
-		currentminx += 1
-		currentmaxx -= 1
-		currentminz += 1
-		currentmaxz -= 1
-		currentminy += 3
-		currentmaxy += 3
+	currentSection = Section(
+		box.minx,
+		box.maxx if (box.maxx - box.minx) % 2 == 1 else box.maxx - 1,
+		box.minz,
+		box.maxz if (box.maxz - box.minz) % 2 == 1 else box.maxz - 1,
+		box.miny + 1,
+		box.miny + 4
+	)
+	while(currentSection.maxy <= maxHeight and currentSection.maxx - currentSection.minx >= 3 and currentSection.maxz - currentSection.minz >= 3):
+		sections.append(currentSection)
+		currentSection = Section(
+			currentSection.minx + 1,
+			currentSection.maxx - 1,
+			currentSection.minz + 1,
+			currentSection.maxz - 1,
+			currentSection.miny + 3,
+			currentSection.maxy + 3
+		)
 	return sections
 
 def generateFloor(level, box):
-	fillArea(level, (4, 0), 
-		box.minx, 
-		box.maxx if (box.maxx - box.minx) % 2 == 1 else box.maxx - 1, 
-		box.minz, 
-		box.maxz if (box.maxz - box.minz) % 2 == 1 else box.maxz - 1, 
-		box.miny, 
-		box.miny + 1
+	fillSection(level, (4, 0), Section(
+			box.minx, 
+			box.maxx if (box.maxx - box.minx) % 2 == 1 else box.maxx - 1, 
+			box.minz, 
+			box.maxz if (box.maxz - box.minz) % 2 == 1 else box.maxz - 1, 
+			box.miny, 
+			box.miny + 1
+		)
 	)
 
 def generateSections(level, sections):
@@ -67,16 +81,21 @@ def generatePillars(level, section):
 			utilityFunctions.setBlock(level, (43, 5), x, section.miny + 1, z)
 
 def generateCeilings(level, section):
-	fillArea(level, (43, 5), section.minx, section.maxx, section.minz, section.maxz, section.maxy - 1, section.maxy)
+	section = Section(
+		section.minx,
+		section.maxx,
+		section.minz,
+		section.maxz,
+		section.maxy - 1,
+		section.maxy
+	)
+	fillSection(level, (43, 5), section)
 
 def clearSection(level, section):
-	clearArea(level, section.minx, section.maxx, section.minz, section.maxz, section.miny, section.maxy)
+	fillSection(level, (0, 0), section)
 
-def clearArea(level, minx, maxx, minz, maxz, miny, maxy):
-	fillArea(level, (0, 0), minx, maxx, minz, maxz, miny, maxy)
-
-def fillArea(level, blockType, minx, maxx, minz, maxz, miny, maxy):
-	for x in range(minx, maxx):
-		for z in range(minz, maxz):
-			for y in range(miny, maxy):
+def fillSection(level, blockType, section):
+	for x in range(section.minx, section.maxx):
+		for z in range(section.minz, section.maxz):
+			for y in range(section.miny, section.maxy):
 					utilityFunctions.setBlock(level, blockType, x, y, z)
