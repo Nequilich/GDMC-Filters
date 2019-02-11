f606a232444d8d4977702691c9e7d0fa81ce2df5
Nekaerth <lasse.j.lundin@gmail.com>
2019-02-11 21:15:55 +0100

    test_surface_02 now calculates steepness
    Height and steepness is also printed in console.

--- test_surface_02.py
+++ test_surface_02.py
@@ -1,14 +1,13 @@
 def perform(level, box, options):
-	neighborHeightAtRowStart = 256
-	for x in range(box.minx, box.maxx):
-		y = getSurfaceBlockHeightViaStartPoint(level, x, box.minz, neighborHeightAtRowStart)
-		setBlock(level, x, y + 1, box.minz, 20)
-		neighborHeight = y
-		neighborHeightAtRowStart = y
-		for z in range(box.minz + 1, box.maxz):
-			y = getSurfaceBlockHeightViaStartPoint(level, x, z, neighborHeight)
-			setBlock(level, x, y + 1, z, 20)
-			neighborHeight = y
+	surface = getSurface(level, box)
+	setSteepness(box, surface)
+	print("Height:")
+	printSurfaceHeight(box, surface)
+	print("Steepness:")
+	printSurfaceSteepness(box, surface)
+	for x in range(0, box.maxx - box.minx):
+		for z in range(0, box.maxz - box.minz):
+			setBlock(level, x + box.minx, surface[x][z].height + 1, z + box.minz, 20)
 
 aboveSurfaceBlocks = [0, 6, 17, 18, 31, 32, 37, 38, 39, 40, 59, 81, 83, 99, 100, 103, 104, 105, 106, 111, 141, 142, 161, 162, 175]
 def isSurfaceBlock(level, x, y, z):
@@ -49,3 +48,64 @@ def getSurfaceBlockHeightViaStartPoint(level, x, z, startHeight):
 			else:
 				surfaceIsFound = True
 		return y - 1
+
+def getSurface(level, box):
+	surface = []
+	neighborHeightAtRowStart = 256
+	for x in range(box.minx, box.maxx):
+		y = getSurfaceBlockHeightViaStartPoint(level, x, box.minz, neighborHeightAtRowStart)
+		row = [Block(y)]
+		neighborHeight = y
+		neighborHeightAtRowStart = y
+		for z in range(box.minz + 1, box.maxz):
+			y = getSurfaceBlockHeightViaStartPoint(level, x, z, neighborHeight)
+			row.append(Block(y))
+			neighborHeight = y
+		surface.append(row)
+	return surface
+
+class Block:
+
+	def __init__(self, height, steepness = 0):
+		self.height = height
+		self.steepness = steepness
+
+def getSteepness(box, surface, x, z):
+	blocks = []
+	if (x + 1 < box.maxx - box.minx):
+		blocks.append(surface[x + 1][z])
+	if (z + 1 < box.maxz - box.minz):
+		blocks.append(surface[x][z + 1])
+	if (x - 1 >= 0):
+		blocks.append(surface[x - 1][z])
+	if (z - 1 >= 0):
+		blocks.append(surface[x][z - 1])
+	
+	minHeight = surface[x][z].height
+	maxHeight = surface[x][z].height
+	for block in blocks:
+		h = block.height
+		if (h < minHeight):
+			minHeight = h
+		if (h > maxHeight):
+			maxHeight = h
+	return maxHeight - minHeight
+
+def setSteepness(box, surface):
+	for x in range(0, box.maxx - box.minx):
+		for z in range(0, box.maxz - box.minz):
+			surface[x][z].steepness = getSteepness(box, surface, x, z)
+
+def printSurfaceHeight(box, surface):
+	for x in range(0, box.maxx - box.minx):
+		s = ""
+		for z in range(0, box.maxz - box.minz):
+			s += str(surface[x][z].height) + " "
+		print(s)
+
+def printSurfaceSteepness(box, surface):
+	for x in range(0, box.maxx - box.minx):
+		s = ""
+		for z in range(0, box.maxz - box.minz):
+			s += str(surface[x][z].steepness) + " "
+		print(s)
