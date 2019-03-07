1353e85ea1fb6a95f18d6b5f4e6ac3faee906848
Nekaerth <lasse.j.lundin@gmail.com>
2019-03-07 13:45:35 +0100

    Added getMatrix in Common

--- SettlementGenerator/Common.py
+++ SettlementGenerator/Common.py
@@ -1,3 +1,12 @@
 def setBlock(level, x, y, z, block, data = 0):
 	level.setBlockAt(x, y, z, block)
 	level.setBlockDataAt(x, y, z, data)
+
+def getMatrix(height, width, defaultValue):
+	matrix = []
+	for x in range(height):
+		row = []
+		for z in range(width):
+			row.append(defaultValue)
+		matrix.append(row)
+	return matrix
