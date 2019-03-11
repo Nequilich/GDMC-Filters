0819b885de38d1ef7d4d8efc322d2ff47859f36d
Nekaerth <lasse.j.lundin@gmail.com>
2019-03-11 03:23:26 +0100

    Implemented simple genetic algorithm

--- Main_Lasse.py
+++ Main_Lasse.py
@@ -1,18 +1,113 @@
-from Classes import Surface
-from Common import setBlock
-from SurfaceManager import calculateHeightMapAdv
-from GetRectangle import getRectangle
+from random import randint
+import random
 
 def perform(level, box, options):
-	surface = Surface(box.minx, box.minz, box.maxx, box.maxz)
-	calculateHeightMapAdv(level, surface)
-	for x in range(surface.xLength):
-		for z in range(surface.zLength):
-			y = surface.surfaceMap[x][z].height
-			setBlock(level, x + surface.xStart, y + 1, z + surface.zStart, 20)
+	goal = "unicorn"
+	size = 20
+	mutationRate = 0.02
+	population = Population(size)
+	createRandomPopulation(population)
+	print("Start:")
+	generation = 1
+	generationEnd = 1000
+	stop = False
+	while generation <= generationEnd and not stop:
+		calculateFitness(population, goal)
+		printPopulation(population, generation)
+		stop = goalIsFound(population, goal)
+		population = createNewPopulation(population, mutationRate)
+		generation += 1
+	print("Generation: " + str(generation - 1))
 
-	rec = getRectangle(surface, int(surface.xLength / 2), int(surface.zLength / 2), 1)
-	for x in range(rec.xStart, rec.xEnd):
-		for z in range(rec.zStart, rec.zEnd):
-			y = surface.surfaceMap[x][z].height
-			setBlock(level, x + surface.xStart, y + 1, z + surface.zStart, 95, 3)
+def createRandomPopulation(population):
+	for i in range(population.size):
+		s = ""
+		for j in range(7):
+			s += getRandomChar()
+		population.elements.append(Element(s))
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
+def getRandomChar():
+	return chars[randint(0, 52)]
+
+def calculateFitness(population, goal):
+	for element in population.elements:
+		fitness = 1
+		for i in range(7):
+			if goal[i] == element.body[i]:
+				fitness += 2
+		element.fitness = fitness
+
+def createNewPopulation(population, mutationRate):
+	newPopulation = Population(population.size)
+	for element in population.elements:
+		parents = selectParents(population)
+		s = ""
+		for i in range(7):
+			isMutating = random.uniform(0.0, 1.0) <= mutationRate
+			if isMutating:
+				s += getRandomChar()
+			else:
+				r = randint(0, 1)
+				s += parents[r].body[i]
+		newPopulation.elements.append(Element(s))
+	return newPopulation
+
+def selectParents(population):
+	parents = []
+	totalFitness = 0
+	for element in population.elements:
+		totalFitness += element.fitness
+
+	r = randint(0, totalFitness - 1)
+	firstParentIndex = 0
+	f = 0
+	i = 0
+	while i < population.size:
+		element = population.elements[i]
+		f += element.fitness
+		if r < f:
+			totalFitness -= element.fitness
+			firstParentIndex = i
+			parents.append(element)
+			break
+		i += 1
+
+	r = randint(0, totalFitness - 1)
+	f = 0
+	i = 0
+	while i < population.size:
+		if i == firstParentIndex:
+			i += 1
+			continue
+		element = population.elements[i]
+		f += element.fitness
+		if r < f:
+			parents.append(element)
+			break
+		i += 1
+	return parents
+
+def printPopulation(population, generation):
+	s = "Generation " + str(generation) + ": "
+	for element in population.elements:
+		s += "(" + element.body + ", " + str(element.fitness) + "), "
+	print(s[:-2])
+
+def goalIsFound(population, goal):
+	for element in population.elements:
+		if element.body == goal:
+			return True
+	return False
+
+class Population:
+
+	def __init__(self, size):
+		self.size = size
+		self.elements = []
+
+class Element:
+
+	def __init__(self, body):
+		self.body = body
+		self.fitness = 0
