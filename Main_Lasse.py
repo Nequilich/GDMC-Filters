8cd3c6a832abef7d9bed6b7ae52e85d3090707dc
Nekaerth <lasse.j.lundin@gmail.com>
2019-03-24 18:24:05 +0100

    Experimenting with road building

--- Main_Lasse.py
+++ Main_Lasse.py
@@ -1,8 +1,7 @@
 from Classes import Surface
 from Common import setBlock
+from GetPath import getPath
 from SurfaceManager import calculateHeightMapAdv
-from SurfaceManager import calculateSectionMid
-from SurfaceManager import calculateSections
 from SurfaceManager import calculateSteepnessMap
 from SurfaceManager import calculateWaterPlacement
 
@@ -11,20 +10,25 @@ def perform(level, box, options):
 	calculateHeightMapAdv(level, surface)
 	calculateSteepnessMap(surface)
 	calculateWaterPlacement(level, surface)
-	sections = calculateSections(surface, 1, 40)
 
-	for x in range(surface.xLength):
-		for z in range(surface.zLength):
-			height = surface.surfaceMap[x][z].height
-			sectionId = surface.surfaceMap[x][z].sectionId
-			if sectionId == -1:
-				setBlock(level, surface.xStart + x, height + 1, surface.zStart + z, 20)
-			else:
-				setBlock(level, surface.xStart + x, height + 1, surface.zStart + z, 95, sectionId % 15 + 1)
+	path = getPath(surface, 10, 10, surface.xLength - 11, surface.zLength - 11)
 
-	for section in sections:
-		calculateSectionMid(surface, section)
+	for point in path:
+		buildRoadPoint(level, surface, point.x, point.z)
 
-	for section in sections:
-		height = surface.surfaceMap[section.xMid][section.zMid].height
-		setBlock(level, surface.xStart + section.xMid, height + 2, surface.zStart + section.zMid, 57)
+def buildRoadPoint(level, surface, x, z):
+	for p in [(x, z), (x + 1, z), (x - 1, z), (x, z + 1), (x, z - 1)]:
+		if not isWithinBorder(surface, p[0], p[1]) or surface.surfaceMap[p[0]][p[1]].isOccupied or isGreatHeightDifference(surface, x, z, p[0], p[1]):
+			continue
+		height = surface.surfaceMap[p[0]][p[1]].height
+		setBlock(level, surface.xStart + p[0], height, surface.zStart + p[1], 4)
+		surface.surfaceMap[p[0]][p[1]].isOccupied = True
+
+def isWithinBorder(surface, x, z):
+	return x >= 0 and x < surface.xLength and z >= 0 and z < surface.zLength
+
+def isGreatHeightDifference(surface, x1, z1, x2, z2):
+	heigth1 = surface.surfaceMap[x1][z1].height
+	heigth2 = surface.surfaceMap[x2][z2].height
+	difference = heigth1 - heigth2
+	return difference > 1 or difference < -1
