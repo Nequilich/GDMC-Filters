c326151a44574d3bfc5e30ee25f2938e0046112f
nequilich <nequilich@gmail.com>
2019-03-25 15:17:18 +0100

    Update Main_Daniel.py

--- Main_Daniel.py
+++ Main_Daniel.py
@@ -53,10 +53,47 @@ def perform(level, box, options):
 	for path in paths:
 		for p in path:
 			y = surface.surfaceMap[p.x][p.z].height
-			setBlock(level, p.x + surface.xStart, y, p.z + surface.zStart, 57, 0)
+			point = Point(p.x + surface.xStart, p.z + surface.zStart)
+			buildPathPoint(level, point, y)
 
 def getSection(sections, id):
 	for section in sections:
 		if section.id == id:
 			return section
 	return None
+
+def buildPathPoint(level, point, height):
+	buildCenterPathTile(level, point, height)
+	for p in [(point.x - 1, point.z), (point.x + 1, point.z), (point.x, point.z - 1), (point.x, point.z + 1)]:
+		buildOuterPathTile(level, Point(p[0], p[1]), height)
+
+def buildCenterPathTile(level, point, height):
+	setBlock(level, point.x, height, point.z, 43, 0)
+	clearAboveTile(level, point, height)
+
+def buildOuterPathTile(level, point, height):
+	if level.blockAt(point.x, height, point.z) == 43:
+		return
+	if level.blockAt(point.x, height, point.z) == 0:
+		i = 0
+		while level.blockAt(point.x, height - i, point.z) == 0:
+			if level.blockAt(point.x, height - 1 - i, point.z) == 43:
+				return
+			i += 1
+	
+	if level.blockAt(point.x, height + 1, point.z) == 4:
+		setBlock(level, point.x, height + 1, point.z, 44, 3)
+	if level.blockAt(point.x, height - 1, point.z) == 4:
+		setBlock(level, point.x, height, point.z, 44, 3)
+	else:
+		setBlock(level, point.x, height, point.z, 4, 0)
+	
+	i = 1
+	while(level.blockAt(point.x, height - i, point.z) == 0):
+		setBlock(level, point.x, height - i, point.z, 98, 0)
+		i += 1
+
+def clearAboveTile(level, point, height):
+	for i in range(1, 6):
+		setBlock(level, point.x, height + i, point.z, 0, 0)
+	
