ecfa0d7553624402bb29c8cefb2e358755c22b9f
Jungne <jungne7@gmail.com>
2019-03-29 16:29:34 +0100

    BiomeFinder Experiments

--- Main_Jungne.py
+++ Main_Jungne.py
@@ -3,10 +3,14 @@ from Classes import Surface
 from SurfaceManager import calculateHeightMapAdv
 from BridgeBuilder import buildBridge
 from BiomeFinder import findBiomes
+from SurfaceManager import calculateSteepnessMap
+from SurfaceManager import calculateWaterPlacement
+from SurfaceManager import calculateSections
+from VillagerSpawner import spawnVillager
 
 inputs = (
     ("Width", 4),
-    ("Material", ("Oak","Cobblestone", "Birch")),
+    ("Material", ("Oak", "Cobblestone", "Birch")),
     ("Creator: Jungne Losang", "label")
 )
 
@@ -34,30 +38,83 @@ birchMaterial = {
     "torch": 50
 }
 
+glass = {
+    "green": (95, 5),
+    "yellow": (95, 4),
+    "orange": (95, 1),
+    "red": (95, 14),
+    "blue": (95, 11)
+}
+
+
 def perform(level, box, options):
+
+
+    #spawnVillager(level, box)
+    
+    
     # BridgeBuilder
-    bridgeWidth = options["Width"]
+    # bridgeWidth = options["Width"]
 
     surface = Surface(box.minx, box.minz, box.maxx, box.maxz)
     calculateHeightMapAdv(level, surface)
-    startPoint = (box.minx, box.minz)
-    endPoint = (box.maxx-1, box.maxz-1)
-    bridgeY = surface.surfaceMap[0][0].height + 1  #+1 to raise above the surface
+    # startPoint = (box.minx, box.minz)
+    # endPoint = (box.maxx-1, box.maxz-1)
+    # bridgeY = surface.surfaceMap[0][0].height + 1  #+1 to raise above the surface
 
-    chosenMaterial = options["Material"]
-    if (chosenMaterial == "Oak"):
-      materials = oakMaterial
-    elif (chosenMaterial == "Cobblestone"):
-      materials = cobblestoneMaterial
-    elif (chosenMaterial == "Birch"):
-      materials = birchMaterial
-    else:
-      materials = oakMaterial
+    # chosenMaterial = options["Material"]
+    # if (chosenMaterial == "Oak"):
+    #   materials = oakMaterial
+    # elif (chosenMaterial == "Cobblestone"):
+    #   materials = cobblestoneMaterial
+    # elif (chosenMaterial == "Birch"):
+    #   materials = birchMaterial
+    # else:
+    #   materials = oakMaterial
 
-    buildBridge(level, startPoint, endPoint, bridgeY, bridgeWidth, materials)
+    # buildBridge(level, startPoint, endPoint, bridgeY, bridgeWidth, materials)
 
     # BiomeFinder
-    # biomes = findBiomes(level, box)
+    biomes = findBiomes(level, box, surface)
 
-    
+    # Topographic marker
+    # surface = Surface(box.minx, box.minz, box.maxx, box.maxz)
+    # calculateHeightMapAdv(level, surface)
+    # calculateSteepnessMap(surface)
+    # calculateWaterPlacement(level, surface)
 
+    # sections = calculateSections(surface, 1, 40)
+
+    # minHeight = 255
+    # maxHeight = 0
+
+    # for x in range(surface.xLength):
+    #     for z in range(surface.zLength):
+    #         height = surface.surfaceMap[x][z].height
+
+    #         if (height < minHeight):
+    #             minHeight = height
+    #         if (height > maxHeight):
+    #             maxHeight = height
+
+    # heightRange = maxHeight - minHeight
+    # heigthInterval = heightRange / 4
+
+    # for x in range(surface.xLength):
+    #     for z in range(surface.zLength):
+    #         height = surface.surfaceMap[x][z].height
+    #         if (surface.surfaceMap[x][z].isWater):
+    #           setBlock(level, surface.xStart + x,
+    #                      height + 1, surface.zStart + z, glass["blue"][0], glass["blue"][1])
+    #         elif (height <= minHeight + heigthInterval):
+    #             setBlock(level, surface.xStart + x,
+    #                      height + 1, surface.zStart + z, glass["green"][0], glass["green"][1])
+    #         elif (height <= minHeight + heigthInterval * 2):
+    #             setBlock(level, surface.xStart + x,
+    #                      height + 1, surface.zStart + z, glass["yellow"][0], glass["yellow"][1])
+    #         elif (height <= minHeight + heigthInterval * 3):
+    #             setBlock(level, surface.xStart + x,
+    #                      height + 1, surface.zStart + z, glass["orange"][0], glass["orange"][1])
+    #         else:
+    #             setBlock(level, surface.xStart + x,
+    #                      height + 1, surface.zStart + z, glass["red"][0], glass["red"][1])
--- SettlementGenerator/BiomeFinder.py
+++ SettlementGenerator/BiomeFinder.py
@@ -1,13 +1,23 @@
 from pymclevel.biome_types import biome_types  # Converts biome id to its name.
 from Common import convertChunkBlockCoordinate
+from Common import setBlock
 
-def findBiomes(level, box):
+def findBiomes(level, box, surface):
     biomesFound = set()
 
     for cPos in box.chunkPositions:
         chunk = level.getChunk(cPos[0], cPos[1])
         biomes = chunk.root_tag["Level"]["Biomes"].value
         biomesFound.update(biomes)
+        # Marks river biome with yellow wool.
+        # for i in range(biomes.size):
+        #     if biomes[i] == 7:  # River
+        #         blockXZ = convertChunkBlockCoordinate(cPos[0], cPos[1], i)
+        #         blockX = blockXZ[0]
+        #         blockZ = blockXZ[1]
+        #         surfaceX = blockX - box.minx
+        #         surfaceZ = blockZ - box.minz
+        #         setBlock(level, blockX, surface.surfaceMap[surfaceX][surfaceZ].height, blockZ, 35, 4)
 
     # Prints the names of the different biomes found.
     print("Biomes found:")
