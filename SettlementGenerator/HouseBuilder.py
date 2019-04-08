1253f8318efe2eaf1e525a13b5aefeff934cd2f1
Nekaerth <lasse.j.lundin@gmail.com>
2019-04-08 16:41:52 +0200

    Houses is now build 1 higher off the ground

--- SettlementGenerator/HouseBuilder.py
+++ SettlementGenerator/HouseBuilder.py
@@ -15,40 +15,40 @@ def buildHouse(level, surface, prop):
 def buildFloor(level, surface, prop):
 	for x in range(prop.xStart + 1, prop.xEnd - 1):
 		for z in range(prop.zStart + 1, prop.zEnd - 1):
-			setBlock(level, x + surface.xStart, prop.height, z + surface.zStart, 4)
+			setBlock(level, x + surface.xStart, prop.height + 1, z + surface.zStart, 4)
 
 def buildWalls(level, surface, prop):
 	for x in range(prop.xStart + 2, prop.xEnd - 2):
-		for y in range(prop.height + 1, prop.height + 4):
+		for y in range(prop.height + 2, prop.height + 5):
 			setBlock(level, x + surface.xStart, y, prop.zStart + 1 + surface.zStart, 5)
 			setBlock(level, x + surface.xStart, y, prop.zEnd - 2 + surface.zStart, 5)
 	for z in range(prop.zStart + 2, prop.zEnd - 2):
-		for y in range(prop.height + 1, prop.height + 4):
+		for y in range(prop.height + 2, prop.height + 5):
 			setBlock(level, prop.xStart + 1 + surface.xStart, y, z + surface.zStart, 5)
 			setBlock(level, prop.xEnd - 2 + surface.xStart, y, z + surface.zStart, 5)
-	for y in range(prop.height + 1, prop.height + 4):
+	for y in range(prop.height + 2, prop.height + 5):
 		setBlock(level, prop.xStart + 1 + surface.xStart, y, prop.zStart + 1 + surface.zStart, 17)
 		setBlock(level, prop.xStart + 1 + surface.xStart, y, prop.zEnd - 2 + surface.zStart, 17)
 		setBlock(level, prop.xEnd - 2 + surface.xStart, y, prop.zStart + 1 + surface.zStart, 17)
 		setBlock(level, prop.xEnd - 2 + surface.xStart, y, prop.zEnd - 2 + surface.zStart, 17)
 
 def buildCeiling(level, surface, prop):
-	y = prop.height + 4
+	y = prop.height + 5
 	for x in range(prop.xStart + 2, prop.xEnd - 2):
 		for z in range(prop.zStart + 2, prop.zEnd - 2):
 			setBlock(level, x + surface.xStart, y, z + surface.zStart, 5)
 
 def buildRoofNS(level, surface, prop):
-	y = prop.height + 4
+	y = prop.height + 5
 	for x in range(prop.xStart + 1, prop.xEnd - 1):
 		setBlock(level, x + surface.xStart, y, prop.zStart + 1 + surface.zStart, 17, 4)
 		setBlock(level, x + surface.xStart, y, prop.zEnd - 2 + surface.zStart, 17, 4)
-	y = prop.height + 5
+	y = prop.height + 6
 	for x in range(prop.xStart + 2, prop.xEnd - 2):
 		setBlock(level, x + surface.xStart, y, prop.zStart + 1 + surface.zStart, 17, 4)
 		setBlock(level, x + surface.xStart, y, prop.zEnd - 2 + surface.zStart, 17, 4)
 	
-	y = prop.height + 4
+	y = prop.height + 5
 	for z in range(prop.zStart + 1, prop.zEnd - 1):
 		setBlock(level, prop.xStart + surface.xStart, y, z + surface.zStart, 17, 8)
 		setBlock(level, prop.xStart + 1 + surface.xStart, y + 1, z + surface.zStart, 17, 8)
@@ -58,16 +58,16 @@ def buildRoofNS(level, surface, prop):
 			setBlock(level, x + surface.xStart, y + 2, z + surface.zStart, 17, 8)
 
 def buildRoofEW(level, surface, prop):
-	y = prop.height + 4
+	y = prop.height + 5
 	for z in range(prop.zStart + 1, prop.zEnd - 1):
 		setBlock(level, prop.xStart + 1 + surface.xStart, y, z + surface.zStart, 17, 8)
 		setBlock(level, prop.xEnd - 2 + surface.xStart, y, z + surface.zStart, 17, 8)
-	y = prop.height + 5
+	y = prop.height + 6
 	for z in range(prop.zStart + 2, prop.zEnd - 2):
 		setBlock(level, prop.xStart + 1 + surface.xStart, y, z + surface.zStart, 17, 8)
 		setBlock(level, prop.xEnd - 2 + surface.xStart, y, z + surface.zStart, 17, 8)
 	
-	y = prop.height + 4
+	y = prop.height + 5
 	for x in range(prop.xStart + 1, prop.xEnd - 1):
 		setBlock(level, x + surface.xStart, y, prop.zStart + surface.zStart, 17, 4)
 		setBlock(level, x + surface.xStart, y + 1, prop.zStart + 1 + surface.zStart, 17, 4)
