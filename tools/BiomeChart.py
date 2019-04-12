889a93d07c88e534ececae63152c2cdccbee71b7
Nekaerth <lasse.j.lundin@gmail.com>
2019-04-12 17:54:00 +0200

    Reverted BimeChart.py changes

--- tools/BiomeChart.py
+++ tools/BiomeChart.py
@@ -13,20 +13,17 @@ def perform(level, box, options):
 	surface = Surface(box.minx, box.minz, box.maxx, box.maxz)
 	calculateHeightMapAdv(level, surface)
 
-	start = time. time()
 	# BiomeFinder adds biome data to surfaceMap
 	findBiomes(level, surface)
-	end = time. time()
-	print(end - start)
 
 	# Makes a colored map of biomes in the sky
-#	height = options["Chart height"]
-#	biomeIds = Biomes.getBiomeDict()
-#	biomeNameList = Biomes.biomes.keys()
-#	for x in range(surface.xLength):
-#		for z in range(surface.zLength):
-#			biomeName = biomeIds[surface.surfaceMap[x][z].biomeId]
+	height = options["Chart height"]
+	biomeIds = Biomes.getBiomeDict()
+	biomeNameList = Biomes.biomes.keys()
+	for x in range(surface.xLength):
+		for z in range(surface.zLength):
+			biomeName = biomeIds[surface.surfaceMap[x][z].biomeId]
 
-#			for i in range(len(biomeNameList)):
-#				if biomeName == biomeNameList[i]:
-#					setBlock(level, surface, x, height, z, 35, i)
+			for i in range(len(biomeNameList)):
+				if biomeName == biomeNameList[i]:
+					setBlock(level, surface, x, height, z, 35, i)
