0b82e738522b78533c5cd323e4308c2b80e0dddc
Nekaerth <lasse.j.lundin@gmail.com>
2019-04-12 17:50:08 +0200

    Update RemoveTree.py

--- SettlementGenerator/RemoveTree.py
+++ SettlementGenerator/RemoveTree.py
@@ -19,6 +19,8 @@ def removeTree(level, x, y, z):
 
 		# Adds neighbors to stack
 		blocks.extend([Block(x + 1, y, z), Block(x - 1, y, z), Block(x, y + 1, z), Block(x, y - 1, z), Block(x, y, z + 1), Block(x, y, z - 1)])
+		blocks.extend([Block(x + 1, y + 1, z), Block(x - 1, y + 1, z), Block(x, y + 1, z + 1), Block(x, y + 1, z - 1)])
+		blocks.extend([Block(x + 1, y - 1, z), Block(x - 1, y - 1, z), Block(x, y - 1, z + 1), Block(x, y - 1, z - 1)])
 
 treeBlocks = [17, 18, 99, 100, 161, 162]
 def isTreeBlock(level, x, y, z):
