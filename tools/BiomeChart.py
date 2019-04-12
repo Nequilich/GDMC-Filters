b936d7f327b3a73a299bd9d15103eb82b81dfe68
Nekaerth <lasse.j.lundin@gmail.com>
2019-04-12 17:38:20 +0200

    BiomeFinder update

--- tools/BiomeChart.py
+++ tools/BiomeChart.py
@@ -3,26 +3,30 @@ from Classes import Surface
 from SurfaceManager import calculateHeightMapAdv
 from BiomeFinder import findBiomes
 import Biomes
+import time
 
 inputs = (
-    ("Chart height", 100),
+	("Chart height", 100),
 )
 
 def perform(level, box, options):
-    surface = Surface(box.minx, box.minz, box.maxx, box.maxz)
-    calculateHeightMapAdv(level, surface)
+	surface = Surface(box.minx, box.minz, box.maxx, box.maxz)
+	calculateHeightMapAdv(level, surface)
 
-    # BiomeFinder adds biome data to surfaceMap
-    findBiomes(level, surface)
+	start = time. time()
+	# BiomeFinder adds biome data to surfaceMap
+	findBiomes(level, surface)
+	end = time. time()
+	print(end - start)
 
-    # Makes a colored map of biomes in the sky
-    height = options["Chart height"]
-    biomeIds = Biomes.getBiomeDict()
-    biomeNameList = Biomes.biomes.keys()
-    for x in range(surface.xLength):
-        for z in range(surface.zLength):
-            biomeName = biomeIds[surface.surfaceMap[x][z].biomeId]
+	# Makes a colored map of biomes in the sky
+#	height = options["Chart height"]
+#	biomeIds = Biomes.getBiomeDict()
+#	biomeNameList = Biomes.biomes.keys()
+#	for x in range(surface.xLength):
+#		for z in range(surface.zLength):
+#			biomeName = biomeIds[surface.surfaceMap[x][z].biomeId]
 
-            for i in range(len(biomeNameList)):
-                if (biomeName == biomeNameList[i]):
-                    setBlock(level, surface, x, height, z, 35, i)
+#			for i in range(len(biomeNameList)):
+#				if biomeName == biomeNameList[i]:
+#					setBlock(level, surface, x, height, z, 35, i)
