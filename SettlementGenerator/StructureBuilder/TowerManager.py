7fb9e5eb19d5f73a82b4392859f87bed315ea3f7
nequilich <nequilich@gmail.com>
2019-04-13 17:54:55 +0200

    Update TowerManager.py

--- SettlementGenerator/StructureBuilder/TowerManager.py
+++ SettlementGenerator/StructureBuilder/TowerManager.py
@@ -4,10 +4,10 @@ from StructureBuilder import Base
 from StructureBuilder import Blueprint
 
 def getTowerBlueprint(point, baseHeight):
-	filePath = "./stock-filters/SettlementGenerator/structures/medium_tower.json"
+	filePath = "./stock-filters/SettlementGenerator/StructureBuilder/structures/tower_medium.json"
 	blockRegister = loadFile(filePath)
 	base = Base(point.x + 2, point.z + 2, 7, 7, baseHeight)
-	return Blueprint(point, blockRegister, baseHeight, base)
+	return Blueprint(point, blockRegister, baseHeight, base, True)
 
 def loadFile(filePath):
 	with open(filePath) as f:
