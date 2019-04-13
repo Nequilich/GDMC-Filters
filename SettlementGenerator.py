305fa82ce5c6af970805d7114f54e77ebf4c6cdd
Nekaerth <lasse.j.lundin@gmail.com>
2019-04-13 13:59:30 +0200

    Created PathManager.py

--- SettlementGenerator.py
+++ SettlementGenerator.py
@@ -1,4 +1,3 @@
-from random import randint
 from Classes import Edge
 from Classes import Node
 from Classes import Point
@@ -8,12 +7,11 @@ from Common import setBlock
 from FarmBuilder import buildPatch
 from FarmBuilder import clearPatchProperty
 from GetPath import getPath
-from GetPathBetweenSections import getPathBetweenSections
 from GetPropertiesAlongPath import getPropertiesAlongPath
 from HouseBuilder import buildHouse
 from HouseBuilder import clearHouseProperty
-from Kruskal import getMinimumSpanningTree
-from RemoveTree import removeTree
+from PathManager import getPathsBetweenSections
+from PathManager import getPathsInSection
 from RoadBuilder import buildTestRoad
 from SurfaceManager import calculateHeightMapAdv
 from SurfaceManager import calculateSteepnessMap
@@ -32,7 +30,7 @@ def perform(level, box, options):
 		if not section.isWater:
 			calculateSectionMid(surface, section)
 			landSections.append(section)
-	paths = getPathBetweenSections(surface, landSections)
+	paths = getPathsBetweenSections(surface, landSections)
 
 	for section in landSections:
 		paths.extend(getPathsInSection(surface, section))
@@ -62,47 +60,6 @@ def perform(level, box, options):
 
 	for p in patchProperties:
 		buildPatch(level, surface, p)
-
-def getPathsInSection(surface, section):
-	amountOfPoints = len(section.points)
-	amountOfNewNodes = amountOfPoints / 5000 - 1
-	if amountOfNewNodes < 1:
-		return []
-	points = [Point(section.xMid, section.zMid)]
-	for _ in range(amountOfNewNodes):
-		i = randint(0, amountOfPoints - 1)
-		p = section.points[i]
-		points.append(Point(p.x, p.z))
-
-	nodes = []
-	for i, point in enumerate(points):
-		nodes.append(Node(i, point))
-
-	edges = []
-	for node in nodes:
-		for otherNode in nodes:
-			if otherNode.id <= node.id:
-				continue
-			point1 = node.data
-			point2 = otherNode.data
-			cost = getEuclideanDistance(surface, point1, point2)
-			edges.append(Edge(cost, node.id, otherNode.id))
-
-	minimumSpanningTree = getMinimumSpanningTree(nodes, edges)
-
-	paths = []
-	for edge in minimumSpanningTree:
-		point1 = getNode(nodes, edge.nodeId1).data
-		point2 = getNode(nodes, edge.nodeId2).data
-		path = getPath(surface, point1.x, point1.z, point2.x, point2.z)
-		paths.append(path)
-	return paths
-
-def getNode(nodes, id):
-	for node in nodes:
-		if node.id == id:
-			return node
-	return None
 
 def buildPathway(level, surface, xStart, zStart, xEnd, zEnd):
 	path = getPath(surface, xStart, zStart, xEnd, zEnd)
---
+++ SettlementGenerator/PathManager.py
@@ -0,0 +1,56 @@
+from random import randint
+from Classes import Edge
+from Classes import Node
+from Classes import Point
+from Common import getEuclideanDistance
+from GetPath import getPath
+from Kruskal import getMinimumSpanningTree
+
+def getPathsBetweenSections(surface, sections):
+	points = []
+	for section in sections:
+		points.append(Point(section.xMid, section.zMid))
+	return getPathBetweenPoints(surface, points)
+
+def getPathsInSection(surface, section):
+	points = [Point(section.xMid, section.zMid)]
+	amountOfPoints = len(section.points)
+	amountOfNewNodes = amountOfPoints / 5000 - 1
+	if amountOfNewNodes < 1:
+		return []
+	for _ in range(amountOfNewNodes):
+		i = randint(0, amountOfPoints - 1)
+		p = section.points[i]
+		points.append(Point(p.x, p.z))
+	return getPathBetweenPoints(surface, points)
+
+def getPathBetweenPoints(surface, points):
+	nodes = []
+	for i, point in enumerate(points):
+		nodes.append(Node(i, point))
+
+	edges = []
+	for node in nodes:
+		for otherNode in nodes:
+			if otherNode.id <= node.id:
+				continue
+			point1 = node.data
+			point2 = otherNode.data
+			cost = getEuclideanDistance(surface, point1, point2)
+			edges.append(Edge(cost, node.id, otherNode.id))
+
+	minimumSpanningTree = getMinimumSpanningTree(nodes, edges)
+
+	paths = []
+	for edge in minimumSpanningTree:
+		point1 = getNode(nodes, edge.nodeId1).data
+		point2 = getNode(nodes, edge.nodeId2).data
+		path = getPath(surface, point1.x, point1.z, point2.x, point2.z)
+		paths.append(path)
+	return paths
+
+def getNode(nodes, id):
+	for node in nodes:
+		if node.id == id:
+			return node
+	return None
