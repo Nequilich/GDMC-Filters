dc9ec3a7496394fde65aca2a2ed790c83a272b46
Nekaerth <lasse.j.lundin@gmail.com>
2019-05-03 16:37:26 +0200

    Updated BiomeChart.py and SteepnessChart.py

--- tools/BiomeChart.py
+++ tools/BiomeChart.py
@@ -1,28 +1,40 @@
-from Common import setBlock
-from Classes import Surface
-from SurfaceManager import calculateHeightMapAdv
 from BiomeFinder import findBiomes
-import Biomes
-
-inputs = (
-	("Chart height", 100),
-)
+from Classes import Surface
+from Common import setBlock
+from SurfaceManager import calculateHeightMapAdv
+from SurfaceManager import calculateSteepnessMap
 
 def perform(level, box, options):
 	surface = Surface(box.minx, box.minz, box.maxx, box.maxz)
 	calculateHeightMapAdv(level, surface)
-
-	# BiomeFinder adds biome data to surfaceMap
+	calculateSteepnessMap(surface)
 	findBiomes(level, surface)
 
-	# Makes a colored map of biomes in the sky
-	height = options["Chart height"]
-	biomeIds = Biomes.getBiomeDict()
-	biomeNameList = Biomes.biomes.keys()
 	for x in range(surface.xLength):
 		for z in range(surface.zLength):
-			biomeName = biomeIds[surface.surfaceMap[x][z].biomeId]
+			placeBiomeBlocks(level, surface, x, z)
 
-			for i in range(len(biomeNameList)):
-				if biomeName == biomeNameList[i]:
-					setBlock(level, surface, x, height, z, 35, i)
+d = {
+	0: (9, 0),
+	1: (2, 0),
+	2: (12, 0),
+	3: (35, 7),
+	4: (18, 0),
+	5: (18, 1),
+	6: (35, 13),
+	7: (35, 3),
+	16: (19, 0),
+	17: (24, 0),
+	18: (18, 0),
+	19: (18, 1),
+	20: (35, 7),
+	21: (18, 3),
+	22: (18, 3),
+	23: (18, 3),
+	27: (18, 2),
+	28: (18, 2)
+	}
+def placeBiomeBlocks(level, surface, x, z):
+	biomeId = surface.surfaceMap[x][z].biomeId
+	blockType = d.get(biomeId, (35, 15))
+	setBlock(level, surface, x, 250, z, blockType)
--- tools/SteepnessChart.py
+++ tools/SteepnessChart.py
@@ -1,27 +1,23 @@
 from Classes import Surface
 from Common import setBlock
 from SurfaceManager import calculateHeightMapAdv
-from SurfaceManager import calculateSectionMid
-from SurfaceManager import calculateSections
 from SurfaceManager import calculateSteepnessMap
-from SurfaceManager import calculateWaterPlacement
 
 def perform(level, box, options):
 	surface = Surface(box.minx, box.minz, box.maxx, box.maxz)
 	calculateHeightMapAdv(level, surface)
 	calculateSteepnessMap(surface)
-	calculateWaterPlacement(level, surface)
-	sections = calculateSections(surface, 1, 15)
-	
-	for section in sections:
-		calculateSectionMid(surface, section)
-	
-	blockDatas = [5, 4, 1, 14]
 
 	for x in range(surface.xLength):
 		for z in range(surface.zLength):
-			if surface.surfaceMap[x][z].isWater:
-				setBlock(level, surface, x, 160, z, 35, 15)
-				continue
-			steepness = surface.surfaceMap[x][z].steepness
-			setBlock(level, surface, x, 160, z, 35, blockDatas[min(steepness, 3)])
+			placeSteepnessBlocks(level, surface, x, z)
+
+d = {
+	0: (35, 0),
+	1: (35, 8),
+	2: (35, 7)
+	}
+def placeSteepnessBlocks(level, surface, x, z):
+	steepness = surface.surfaceMap[x][z].steepness
+	blockType = d.get(steepness, (14, 0))
+	setBlock(level, surface, x, 250, z, blockType)
