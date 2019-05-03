3d72c0a809b7f7475934164195614851e6612852
Nekaerth <lasse.j.lundin@gmail.com>
2019-05-03 15:53:52 +0200

    Made analyze filter for poster

--- Main_Lasse.py
+++ Main_Lasse.py
@@ -1,46 +1,52 @@
+from BiomeFinder import findBiomes
+from Biomes import getBiomeDict
 from Classes import Surface
 from Common import setBlock
-from GetPath import getPath
-from PathManager import getPathsBetweenSections
-from PathManager import getPathsInSections
 from SurfaceManager import calculateHeightMapAdv
+from SurfaceManager import calculateSections
 from SurfaceManager import calculateSteepnessMap
 from SurfaceManager import calculateWaterPlacement
-from SurfaceManager import calculateSections
-from SurfaceManager import calculateSectionMid
 
 def perform(level, box, options):
 	surface = Surface(box.minx, box.minz, box.maxx, box.maxz)
 	calculateHeightMapAdv(level, surface)
 	calculateSteepnessMap(surface)
 	calculateWaterPlacement(level, surface)
-	sections = calculateSections(surface, 1, 250)
-	landSections = []
-	for section in sections:
-		if not section.isWater:
-			calculateSectionMid(surface, section)
-			landSections.append(section)
-
-	paths = getPathsInSections(surface, landSections)
-	intersectionPaths = getPathsBetweenSections(surface, landSections)
+	findBiomes(level, surface)
+	sections = calculateSections(surface, 1, 15)
 
 	for x in range(surface.xLength):
 		for z in range(surface.zLength):
-			height = surface.surfaceMap[x][z].height
-			if surface.surfaceMap[x][z].isWater:	
-				setBlock(level, surface, x, height, z, 22)
-			elif surface.surfaceMap[x][z].sectionId == -1:	
-				setBlock(level, surface, x, height, z, 35, 15)
-			else:
-				i = surface.surfaceMap[x][z].sectionId
-				setBlock(level, surface, x, height, z, 35, i % 15)
+			placeSteepnessBlocks(level, surface, x, 250, z)
+			placeBiomeBlocks(level, surface, x, 251, z)
 
-	for path in paths:
-		for p in path:
-			height = surface.surfaceMap[p.x][p.z].height
-			setBlock(level, surface, p.x, height + 1, p.z, 41)
+d1 = {0: (35, 0), 1: (35, 8), 2: (35, 7)}
+def placeSteepnessBlocks(level, surface, x, y, z):
+	steepness = surface.surfaceMap[x][z].steepness
+	blockType = d1.get(steepness, (35, 15))
+	setBlock(level, surface, x, y, z, blockType)
 
-	for path in intersectionPaths:
-		for p in path:
-			height = surface.surfaceMap[p.x][p.z].height
-			setBlock(level, surface, p.x, height + 2, p.z, 57)
+d2 = {
+	0: (9, 0),
+	1: (2, 0),
+	2: (12, 0),
+	3: (35, 7),
+	4: (18, 0),
+	5: (18, 1),
+	6: (35, 13),
+	7: (35, 3),
+	16: (19, 0),
+	17: (24, 0),
+	18: (18, 0),
+	19: (18, 1),
+	20: (35, 7),
+	21: (18, 3),
+	22: (18, 3),
+	23: (18, 3),
+	27: (18, 2),
+	28: (18, 2)
+	}
+def placeBiomeBlocks(level, surface, x, y, z):
+	biomeId = surface.surfaceMap[x][z].biomeId
+	blockType = d2.get(biomeId, (35, 15))
+	setBlock(level, surface, x, y, z, blockType)
