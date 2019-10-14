5dc68b582e5ac78754512b3d3de29fb0ad6e741d
Nekaerth <lasse.j.lundin@gmail.com>
2019-10-14 15:33:11 +0200

    Started work on new bridge algorithm

--- Main_Lasse.py
+++ Main_Lasse.py
@@ -1,52 +1,33 @@
-from Biomes import getBiomeDict
+import math
+
 from Classes import Surface
 from Common import setBlock
-from SurfaceManager import calculateBiomeMap
 from SurfaceManager import calculateHeightMapAdv
-from SurfaceManager import calculateSections
-from SurfaceManager import calculateSteepnessMap
-from SurfaceManager import calculateWaterPlacement
 
 def perform(level, box, options):
 	surface = Surface(box.minx, box.minz, box.maxx, box.maxz)
 	calculateHeightMapAdv(level, surface)
-	calculateSteepnessMap(surface)
-	calculateWaterPlacement(level, surface)
-	calculateBiomeMap(level, surface)
-	sections = calculateSections(surface, 1, 15)
 
-	for x in range(surface.xLength):
-		for z in range(surface.zLength):
-			placeSteepnessBlocks(level, surface, x, 250, z)
-			placeBiomeBlocks(level, surface, x, 251, z)
+	xStart = 2
+	zStart = 2
+	xEnd = surface.xLength - 3
+	zEnd = surface.zLength - 3
+	x = xStart
+	z = zStart
+	y = 90
+	
+	xChange = 1
+	if xStart > xEnd:
+		xChange = -1
+	zChange = 1
+	if zStart > zEnd:
+		zChange = -1
 
-d1 = {0: (35, 0), 1: (35, 8), 2: (35, 7)}
-def placeSteepnessBlocks(level, surface, x, y, z):
-	steepness = surface.surfaceMap[x][z].steepness
-	blockType = d1.get(steepness, (35, 15))
-	setBlock(level, surface, x, y, z, blockType)
+	angle1 = math.atan2(zEnd - z, xEnd - x)
+	angle2 = math.pi
+	angleDif = math.pi - abs(abs(angle1 - angle2) - math.pi)
+	print("a1: " + str(angle1) + ", a2: " + str(angle2) + ", dif: " + str(angleDif))
 
-d2 = {
-	0: (9, 0),
-	1: (2, 0),
-	2: (12, 0),
-	3: (35, 7),
-	4: (18, 0),
-	5: (18, 1),
-	6: (35, 13),
-	7: (35, 3),
-	16: (19, 0),
-	17: (24, 0),
-	18: (18, 0),
-	19: (18, 1),
-	20: (35, 7),
-	21: (18, 3),
-	22: (18, 3),
-	23: (18, 3),
-	27: (18, 2),
-	28: (18, 2)
-	}
-def placeBiomeBlocks(level, surface, x, y, z):
-	biomeId = surface.surfaceMap[x][z].biomeId
-	blockType = d2.get(biomeId, (35, 15))
-	setBlock(level, surface, x, y, z, blockType)
+	#while x != xEnd and z != zEnd:
+	#	angle = math.atan2(zEnd - z, xEnd - x)
+	#setBlock(level, surface, xEnd, y, zEnd, 57)
